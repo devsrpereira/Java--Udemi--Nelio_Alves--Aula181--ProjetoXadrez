867ef313855e232a5f5244b12092b9f6c81771ff
SrDevPereira <123840329+devsrpereira@users.noreply.github.com>
2023-05-11 00:33:05 -0300

    Exception created

--- ChessSystem/src/boardergame/Board.java
+++ ChessSystem/src/boardergame/Board.java
@@ -6,6 +6,9 @@ public class Board {
     private Piece[][] pieces;
 
     public Board(int rows, int columns) {
+        if(rows<1 || columns <1){
+            throw new BoardException("Error no tabuleiro: É necessario ao menos 1 linha e 1 coluna");
+        }
         this.rows = rows;
         this.columns = columns;
         pieces = new Piece[rows][columns];
@@ -14,27 +17,44 @@ public class Board {
     public int getRows() {
         return rows;
     }
-    public void setRows(int rows) {
-        this.rows = rows;
-    }
     public int getColumns() {
         return columns;
     }
-    public void setColumns(int columns) {
-        this.columns = columns;
-    }
 
     public Piece piece(int row, int column){
+        if(!positionExists(row, column)){
+            throw new BoardException("Posição fora do tabuleiro");
+        }
         return pieces[row][column];
     }
 
     public Piece piece(Position position){
+        if(!positionExists(position)){
+            throw new BoardException("Posição fora do tabuleiro");
+        }
         return pieces[position.getRow()][position.getCollumn()];
     }
 
     public void placePiece(Piece piece, Position position){
+        if(thereIsAPiece(position)){
+            throw new BoardException("Já existe uma peça na posição " + position);
+        }
         pieces[position.getRow()][position.getCollumn()] = piece;
         piece.position = position;
     }
 
+    private boolean positionExists(int row, int column){
+        return row >= 0 && row < rows && column >= 0 && column < columns;
+    }
+
+    public boolean positionExists(Position position){
+        return positionExists(position.getRow(), position.getCollumn());
+    }
+
+    public boolean thereIsAPiece(Position position){
+        if(!positionExists(position)){
+            throw new BoardException("Posição fora do tabuleiro");
+        }
+        return piece(position) != null;
+    }
 }
